4f804cbf69d08d7bceff158d570108b6154130f3
CarlosCuautleZac <191G0235@rcarbonifera.tecnm.mx>
2023-05-03 14:20:24 -0600

    icono en el nombre de cada jugador

--- TriviaAPP/Views/FindDeJuego.xaml.cs
+++ TriviaAPP/Views/FindDeJuego.xaml.cs
@@ -7,4 +7,17 @@ public partial class FindDeJuego : ContentPage
 		InitializeComponent();
 		BindingContext= App.ViewModel;
 	}
+
+    private async void btnAnim_Clicked(object sender, EventArgs e)
+    {
+        var b1 = btnAnim.FadeTo(0, 1000, Easing.Linear);
+        var b2 = btnAnim.ScaleTo(2.0, 1000, Easing.BounceIn);
+
+        await Task.WhenAll(b1, b2);
+
+        var b3 = btnAnim.FadeTo(1, 1000, Easing.Linear);
+        var b4 = btnAnim.ScaleTo(1.0, 1000, Easing.BounceOut);
+
+        await Task.WhenAll(b3, b4);
+    }
 }
--- TriviaAPP/Views/Inicio.xaml.cs
+++ TriviaAPP/Views/Inicio.xaml.cs
@@ -7,4 +7,17 @@ public partial class Inicio : ContentPage
 		InitializeComponent();
         BindingContext = App.ViewModel;
     }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        var b1= btnIniciar.FadeTo(0, 1000, Easing.Linear);
+        var b2= btnIniciar.ScaleTo(2.0, 1000, Easing.BounceIn);
+
+        await Task.WhenAll(b1, b2);
+
+        var b3 = btnIniciar.FadeTo(1, 1000, Easing.Linear);
+        var b4 = btnIniciar.ScaleTo(1.0, 1000, Easing.BounceOut);
+
+        await Task.WhenAll(b3, b4);
+    }
 }
