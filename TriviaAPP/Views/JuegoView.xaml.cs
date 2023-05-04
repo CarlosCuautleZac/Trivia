fecf33a467c412517cc00da2edac3b8fee1add4e
CarlosCuautleZac <191G0235@rcarbonifera.tecnm.mx>
2023-05-04 14:13:51 -0600

    Cambios en el diseño

--- TriviaAPP/Views/JuegoView.xaml.cs
+++ TriviaAPP/Views/JuegoView.xaml.cs
@@ -1,3 +1,7 @@
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
+using Button = Microsoft.Maui.Controls.Button;
+
 namespace TriviaAPP.Views;
 
 public partial class JuegoView : ContentPage
@@ -7,4 +11,20 @@ public partial class JuegoView : ContentPage
 		InitializeComponent();
 		BindingContext = App.ViewModel;
 	}
+
+    private async void btnRespuesta_Clicked(object sender, EventArgs e)
+    {
+        //var collectionView = this.FindByName<CollectionView>("CVdeResp");
+        //var btnResp = collectionView.FindByName<Button>("btnRespuesta");
+        var b1 = CVdeResp.FadeTo(1.0, 1000, Easing.Linear);
+        var b2 = CVdeResp.ScaleTo(2.0, 1000, Easing.BounceIn);
+
+        await Task.WhenAll(b1, b2);
+
+        var b3 = CVdeResp.FadeTo(1, 1000, Easing.Linear);
+        var b4 = CVdeResp.ScaleTo(1.0, 1000, Easing.BounceOut);
+
+        await Task.WhenAll(b3, b4);
+
+    }
 }
