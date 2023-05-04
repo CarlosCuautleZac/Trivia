2ae1b2a6aca64ae41961dd38718bbaae7be77c6f
belen-ru <belenruiz911@outlook.com>
2023-05-04 15:27:47 -0600

    Diseño botón inicio

--- TriviaAPP/Views/Inicio.xaml.cs
+++ TriviaAPP/Views/Inicio.xaml.cs
@@ -8,15 +8,28 @@ public partial class Inicio : ContentPage
         BindingContext = App.ViewModel;
     }
 
-    private async void Button_Clicked(object sender, EventArgs e)
+    //private async void Button_Clicked(object sender, EventArgs e)
+    //{
+    //    var b1 = btnIniciar.FadeTo(0, 1000, Easing.Linear);
+    //    var b2 = btnIniciar.ScaleTo(2.0, 1000, Easing.BounceIn);
+
+    //    await Task.WhenAll(b1, b2);
+
+    //    var b3 = btnIniciar.FadeTo(1, 1000, Easing.Linear);
+    //    var b4 = btnIniciar.ScaleTo(1.0, 1000, Easing.BounceOut);
+
+    //    await Task.WhenAll(b3, b4);
+    //}
+
+    private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
     {
-        var b1= btnIniciar.FadeTo(0, 1000, Easing.Linear);
-        var b2= btnIniciar.ScaleTo(2.0, 1000, Easing.BounceIn);
+        var b1 = gridIniciar.FadeTo(0, 1000, Easing.Linear);
+        var b2 = gridIniciar.ScaleTo(2.0, 1000, Easing.BounceIn);
 
         await Task.WhenAll(b1, b2);
 
-        var b3 = btnIniciar.FadeTo(1, 1000, Easing.Linear);
-        var b4 = btnIniciar.ScaleTo(1.0, 1000, Easing.BounceOut);
+        var b3 = gridIniciar.FadeTo(1, 1000, Easing.Linear);
+        var b4 = gridIniciar.ScaleTo(1.0, 1000, Easing.BounceOut);
 
         await Task.WhenAll(b3, b4);
     }
